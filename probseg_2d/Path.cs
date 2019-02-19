6315a79b9b90ed33dd34266c7236630478145b62
gabriela <gabriela@WS30007>
2019-02-19 15:23:18 +0200

    find intersection

--- probseg_2d/Path.cs
+++ probseg_2d/Path.cs
@@ -78,6 +78,7 @@ namespace probseg_2d
             }
         }
 
+
     }
 }
 
--- probseg_2d/Segment.cs
+++ probseg_2d/Segment.cs
@@ -25,6 +25,34 @@ namespace probseg_2d
 
         }
 
+        private bool FindIntersection(Segment segment)
+        {
+            // Get the segments' parameters.
+            double dx12 = End.X - Start.X;
+            double dy12 = End.Y - Start.Y;
+            double dx34 = End.X - Start.X;
+            double dy34 = End.Y - Start.Y;
+
+            // Solve for t1 and t2
+            double denominator = (dy12 * dx34 - dx12 * dy34);
+
+            double t1 =
+                ((Start.X - Start.X) * dy34 + (Start.Y - Start.Y) * dx34)
+                / denominator;
+            if (double.IsInfinity(t1))
+            {
+                // The lines are parallel (or close enough to it).
+                return false;
+            }
+
+            double t2 =
+                ((Start.X - Start.X) * dy12 + (Start.Y - Start.Y) * dx12)
+                / -denominator;
+
+            return
+                ((t1 >= 0) && (t1 <= 1) &&
+                 (t2 >= 0) && (t2 <= 1));
+        }
     }
 
 
